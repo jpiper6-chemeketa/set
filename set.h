e2876431a590fd36adb0e0fe5442d368f137b285
Justin Piper <jpiper6@my.chemeketa.edu>
2026-05-31 21:45:37 +0000

    May 31, 2026, 2:45 PM

--- set.h
+++ set.h
@@ -25,10 +25,13 @@ public:
   Set& operator=(const Set<T>& other)
   {
     if (this != &other) {
+      T* values = new T[other.m_capacity];
+      std::copy(other.m_values, other.m_values + m_size, values);
+
       delete[] m_values;
-      m_values = new T[other.m_size];
+      m_values = values;
       m_size = other.m_size;
-      std::copy(other.m_values, other.m_values + m_size, m_values);
+      m_capacity = other.m_capacity;
     }
     return *this;
   }
