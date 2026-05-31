08979501e8bcadca6bc5ccac8a6a692032df4d69
Justin Piper <jpiper6@my.chemeketa.edu>
2026-05-31 21:31:03 +0000

    May 31, 2026, 2:31 PM

--- set.h
+++ set.h
@@ -46,8 +46,8 @@ public:
 
   void add(const T& value)
   {
-    T* existing = std::find(m_values, m_values + m_capacity, value);
-    if (existing == m_values + m_capacity) {
+    T* existing = std::find(m_values, m_values + m_size, value);
+    if (existing == m_values + m_size) {
       if (m_size == m_capacity) {
         T* values = new T[m_capacity * 2];
 
